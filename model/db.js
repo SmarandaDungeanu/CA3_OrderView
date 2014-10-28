344009a8443fc591e00993efc4ab2cb65b33f782
smarandadungeanu <smaranda.dungeanu@gmail.com>
2014-10-28 13:23:32 +0100

    now we can display the orders

---
+++ model/db.js
@@ -0,0 +1,103 @@
+var mongoose = require( 'mongoose' );
+
+var dbURI = 'mongodb://smara:fotbal@ds033400.mongolab.com:33400/smadb'
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose connected to ' + dbURI);
+});
+
+mongoose.connection.on('error',function (err) {
+    console.log('Mongoose connection error: ' + err);
+});
+
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose disconnected');
+});
+
+process.on('SIGINT', function() {
+    mongoose.connection.close(function () {
+        console.log('Mongoose disconnected through app termination');
+        process.exit(0);
+    });
+});
+
+var OrderSchema = mongoose.Schema({
+    _id: Number,
+    customerId: String,
+    employeeId: Number,
+    orderDate: String,
+    requiredDate: String,
+    shippedDate: String,
+    shipVia: String,
+    freight: Number,
+    shipName: String,
+    shipAddress: String,
+    shipCity: String,
+    shipRegion: String,
+    shipPostalCode: String,
+    shipCountry: String
+});
+
+var CategorySchema = mongoose.Schema({
+    _id: Number,
+    name: String,
+    description: String
+});
+
+var ProductSchema = mongoose.Schema({
+    _id: Number,
+    name: String,
+    categoryId: Number,
+    quantityPerUnit: String,
+    unitPrice: Number,
+    unitsInStock: Number,
+    unitsOnOrder: Number,
+    reorderLevel: Number,
+    discontinued: Number
+});
+
+var DetailsSchema = mongoose.Schema({
+    orderId: Number,
+    productId: Number,
+    unitPrice: Number,
+    quantity: Number,
+    discount: Number
+});
+
+var CustomerSchema = mongoose.Schema({
+    _id: String,
+    companyName: String,
+    contactName: String,
+    contactTitle: String,
+    address: String,
+    city: String,
+    region: String,
+    postalCode: String,
+    country: String,
+    phone: String,
+    fax: String
+});
+
+var EmployeeSchema = mongoose.Schema({
+    _id: Number,
+    lastName: String,
+    firstName: String,
+    title: String,
+    titleOfCourtesy: String,
+    birthDate: String,
+    hireDate: String,
+    address: String,
+    city: String,
+    region: String,
+    postalCode: String,
+    country: String,
+    homePhone: String,
+    extension: String,
+    notes: String
+});
+
+
+mongoose.model( 'Order', OrderSchema);
+mongoose.model( 'Category', CategorySchema);
+mongoose.model( 'Customer', CustomerSchema);
